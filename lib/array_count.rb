efc00d29f3ff2a33e843ed7257d2f66fd00e0732
Andre Powers <andre@stuypend.com>
2021-02-09 16:05:57 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,17 @@
 def count_strings(array)
+  count = 0
+  array.count do |message|
+    count = message.class == String ? count+1 : count+0
+  end
+  return count
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+  count = 0
+  array.count do |message|
+    count = message == "" ? count+1 : count+0
+  end
+  return count
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
